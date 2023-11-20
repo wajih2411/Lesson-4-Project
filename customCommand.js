2eeac4b074687d6461cc01f844237cea741b79d7
wajih2411 <wajihullahm@gmail.com>
2023-11-20 17:29:39 -0600

    Update customCommand.js

--- customCommand.js
+++ customCommand.js
@@ -1,4 +1,11 @@
-Cypress.Commands.add('visitWithCheck', (url, checkSelector) => {
+Cypress.Commands.add('visitAndInteract', (url, action, options = {}) => {
     cy.visit(url);
-    cy.get(checkSelector).should('be.visible'); // Example check
+
+    switch(action) {
+        case 'login':
+            cy.get(options.usernameSelector).type(options.username);
+            cy.get(options.passwordSelector).type(options.password);
+            cy.get(options.submitButtonSelector).click();
+            break;
+    }
 });
